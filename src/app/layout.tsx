dcca61829b3504ee2e2c2b6d6b49ce00a7d28840
emirakbay <akbayemir@gmail.com>
2024-09-25 23:27:41 +0300

    feat: add nextauth session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import { type Session } from "next-auth";
 import { Menu } from "~/components/top-bar";
+import { NextAuthProvider } from "~/providers";
 
 export const metadata = {
   title: "playlistpal",
@@ -12,17 +14,21 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  session,
 }: {
   children: React.ReactNode;
+  session: Session;
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <Menu />
-        <section className="min-h-screen items-center bg-slate-900 text-white sm:flex-col lg:flex-row">
-          {children}
-        </section>
-      </body>
+      <NextAuthProvider session={session}>
+        <body>
+          <Menu />
+          <section className="min-h-screen items-center bg-slate-900 text-white sm:flex-col lg:flex-row">
+            {children}
+          </section>
+        </body>
+      </NextAuthProvider>
     </html>
   );
 }
